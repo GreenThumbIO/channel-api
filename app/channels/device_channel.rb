b006813ff2397b9b95fd48a9accefef57f7e9695
Justin Bowen <JusBowen@gmail.com>
2019-01-16 19:33:43 -0800

    Sending transmissions :outbox_tray:

--- app/channels/device_channel.rb
+++ app/channels/device_channel.rb
@@ -1,16 +1,17 @@
 class DeviceChannel < ApplicationCable::Channel
   def subscribed
     stream_from "device_channel_#{params[:device_id]}"
+    Rails.logger.debug "subscribed cat facts... JK subscribed to device_channel_#{params[:device_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def transmit
+  def message(data)
     # Transmission.create! text: data['message'], device_id: data['device_id']
-    Rails.logger.debug "#{data['message']}"
-    ActionCable.server.broadcast "device_channel_#{params[:device_id]}",
+    Rails.logger.debug "Message Received: #{data['message']}"
+    ActionCable.server.broadcast "device_channel_#{data[:device_id]}",
       message: data['message']
   end
 end
--- harvester/transmit.py
+++ harvester/transmit.py
@@ -2,20 +2,16 @@ from actioncable.connection import Connection
 from actioncable.subscription import Subscription
 from actioncable.message import Message
 import time
-
+import json
 # if __name__ == '__main__':
 connection = Connection(url='ws://cable:28080')
 connection.connect()
 
-subscription = Subscription(connection, identifier={'identifier': 'DeviceChannel'})
-
-def on_receive(message):
-  print('New message arrived!')
-  print('Action: {} | Data: {}'.format(message.action, message.data))
-
-subscription.on_receive(callback=on_receive)
+subscription = Subscription(connection, identifier={'channel':'DeviceChannel'})
 subscription.create()
 
-message = Message(action='transmit', data={'message': "Time: " + str(time.time()), 'device_id': 123})
-
-subscription.send(message)
+subscription.connection.send({
+    'command':'message',
+    'identifier': json.dumps({'channel':'DeviceChannel'}),
+    'data':json.dumps({'action':'message','message':"Time: " + str(time.time())})
+    })
