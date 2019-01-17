b07800ba035c91b433883e7afa48b714e2bcc85e
Justin Bowen <JusBowen@gmail.com>
2019-01-16 21:37:55 -0800

    Streaming sensor data from an serial port

--- harvester/transmit.py
+++ harvester/transmit.py
@@ -1,17 +1,37 @@
 from actioncable.connection import Connection
 from actioncable.subscription import Subscription
 from actioncable.message import Message
+from serial.tools import list_ports
+import serial
 import time
 import json
-# if __name__ == '__main__':
-connection = Connection(url='ws://cable:28080')
+import time.sleep
+
+connection = Connection(url='ws://localhost:28080')
 connection.connect()
 
 subscription = Subscription(connection, identifier={'channel':'DeviceChannel'})
 subscription.create()
+
+def on_receive(message):
+  print('New message arrived!')
+  print('Action: {} | Data: {}'.format(message.action, message.data))
+
+subscription.on_receive(callback=on_receive)
 
 subscription.connection.send({
     'command':'message',
     'identifier': json.dumps({'channel':'DeviceChannel'}),
     'data':json.dumps({'action':'message','message':"Time: " + str(time.time())})
     })
+
+ser = serial.Serial(list_ports.comports()[1].device, 9600)
+while True:
+    time.sleep(1)
+    reading = ser.readline().decode("utf-8")
+    print(reading)
+    subscription.connection.send({
+        'command':'message',
+        'identifier': json.dumps({'channel':'DeviceChannel'}),
+        'data':json.dumps({'action':'message','message': reading})
+        })
